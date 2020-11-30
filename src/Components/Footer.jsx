a01d07d68f50f40759f552209f4e6d1434339b47
Adolfo Herrera <adolfoherrera1417@gmail.com>
2020-11-30 11:31:57 -0600

    Added footer like button

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 
+import { FacebookProvider, Like } from 'react-facebook';
+
 export default function Footer() {
     return (
-        <Container className="nav" fluid style={{marginTop:"2em"}}>
+        <Container className="nav" fluid style={{marginTop:"2em", display:"block"}}>
             <h1 id="comapny-name"> #Party Zone Letters</h1>
+            <FacebookProvider appId="partyzoneletters">
+                <Like href="http://www.facebook.com/partyzoneletters" colorScheme="light" showFaces buttonCount share size="large" layout="button_count" />
+            </FacebookProvider>
         </Container>
     )
 }
